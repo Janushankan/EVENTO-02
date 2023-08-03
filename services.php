891913becb534aa6b2b2b0f68b503a5fc3421ce0
V. Janushankan <vj.janushankan1006@gmail.com>
2023-08-03 16:56:15 +0530

    first commit

---
+++ services.php
@@ -0,0 +1,379 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap"
+        rel="stylesheet">
+
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+        integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- animation effects -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+
+
+    <!-- for footer -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+    <title>Services</title>
+    <link rel="stylesheet" type="text/css" href="css/services.css">
+    <title>Services</title>
+    <link rel="icon" type="image/x-icon" href="images/logo.png">
+
+</head>
+
+<body>
+
+    <!--Navbar-->
+    <nav class="navbar px-5 navbar-expand-lg nav-bg pb-2">
+        <div class="container-fluid">
+            <a href="index.php"><img src="images/logo.png" class="img-fluid img-thumbnail rounded" alt="logo"
+                    style="height: 65px; width: 150px;"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+                <ul class="navbar-nav gap-40">
+                    <li class="nav-item"> <a class="nav-link text-body-emphasis" aria-current="page"
+                            href="index.php">Home </a>
+                    </li>
+                    <li class="nav-item text-body-emphasis"><a class="nav-link text-body-emphasis"
+                            href="about_us.php">About Us</a>
+                    </li>
+                    <!-- <li class="nav-item text-body-emphasis"><a class="nav-link text-body-emphasis" href="#">Categories</a>
+                    </li> -->
+                    <li class="nav-item text-body-emphasis"><a class="nav-link text-body-emphasis active"
+                            href="services.php">Services</a>
+                    </li>
+                    <li class="nav-item text-body-emphasis"><a class="nav-link text-body-emphasis"
+                            href="portfolio.php">Portfolio</a></li>
+                    <li class="nav-item text-body-emphasis"><a class="nav-link text-body-emphasis"
+                            href="packages.php">Packages</a>
+                    </li>
+
+                    <li class="nav-item text-body-emphasis"><a class="nav-link text-body-emphasis"
+                            href="contact_us.php">Contact
+                            Us</a>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+        <nav class="navbar">
+            <div class="container-fluid">
+                <form class="d-flex">
+                    <a href="sign_in.php" class="btn search-btn" style="background-color: #e97dc3;"
+                        type="submit">Login</a>
+                </form>
+            </div>
+        </nav>
+    </nav>
+
+    <main>
+        <!------------------ Services ---------------------------->-
+        <section class="our-services pt-5 pb-4">
+            <div class="container-fluid">
+
+
+                <div class="para">
+                    <h2 class="mt-0" style="color:#e7119c;text-align:left;">Experience and Expertise</h2>
+                    <div class="row">
+                        <div class="card mt-5" style="width: 18.5rem;">
+                            <div class="our-services-col2">
+                                <img src="images/R.jpeg">
+                                <h3 class="card-title">Industry Experience</h3>
+                                <p class="card-text">With over a decade of experience in the event planning industry,
+                                    Evento has successfully
+                                    organized a
+                                    wide range of events across various sectors. Our team has handled birthday parties,
+                                    sports
+                                    tournaments, religious ceremonies, weddings, and social gatherings.</p>
+                                <a href="#" class="ser-btn">Explore</a>
+                            </div>
+                        </div>
+
+
+
+                        <div class="card mt-5" style="width: 18.5rem;">
+                            <div class="our-services-col2">
+                                <img src="images/at.jpeg">
+                                <h3 class="card-title">Attention to Detail</h3>
+                                <p class="card-text">EventPro's planners are known for their meticulous attention to
+                                    detail. We understand that every
+                                    element of an event, from venue selection to decor, audiovisual setup, and guest
+                                    experience,
+                                    contributes to its success. Our team ensure that even the smallest details are taken
+                                    care of to
+                                    create a seamless and immersive event experience.</p>
+                                <a href="#" class="ser-btn">Explore</a>
+                            </div>
+                        </div>
+
+
+
+
+
+                        <div class="card mt-5" style="width: 18.5rem;">
+                            <div class="our-services-col2">
+                                <img src="images/cl.jpeg">
+                                <h3 class="card-title">Creativity</h3>
+                                <p class="card-text">Our team stays abreast of the latest event trends and styles, and
+                                    technological advancements to
+                                    infuse creativity and innovation into every event we plan. We work closely with our
+                                    clients to
+                                    understand their vision and then bring it to life with a imaginative approach.</p>
+                                <a href="#" class="ser-btn">Explore</a>
+                            </div>
+                        </div>
+
+
+
+                        <div class="card mt-5" style="width: 18.5rem;">
+                            <div class="our-services-col2">
+                                <img src="images/ven.jpeg">
+                                <h3 class="card-title">Vendor</h3>
+                                <p class="card-text">Evento has established strong relationships with reputable vendors,
+                                    ensuring access to top-tier
+                                    venues, caterers, entertainers, and suppliers. Our extensive network allows us to
+                                    negotiate
+                                    competitive prices, secure the best services, and manage vendor relationships
+                                    seamlessly.
+
+                                </p>
+                                <a href="#" class="ser-btn">Explore</a>
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="container-fluid pb-5">
+                <div class="para">
+                    <h2 class="mt-5" style="color:#e7119c;text-align:left;">Team Members</h2>
+
+
+                    <div class="row">
+                        <div class="card mt-5" style="width: 18.5rem;">
+                            <div class="our-services-col2">
+                                <img src="images/or.jpeg">
+                                <h3 class="card-title">Event Organizers</h3>
+                                <p class="card-text">Event organizers have a deep understanding of event logistics,
+                                    budgeting, and overall project
+                                    management. They work closely with clients to understand their event goals,
+                                    conceptualize event
+                                    themes, and coordinate all aspects of event planning, from inception to execution.
+                                </p>
+                                <a href="#" class="ser-btn">Explore</a>
+
+                            </div>
+                        </div>
+
+                        <div class="card mt-5" style="width: 18.5rem;">
+                            <div class="our-services-col2">
+                                <img src="images/dec.jpg">
+                                <h3 class="card-title">Decorators</h3>
+                                <p class="card-text">Our creative decorators and event stylists transform event spaces
+                                    into visually captivating
+                                    environments. They have an eye for design and a talent for creating unique and
+                                    memorable event
+                                    aesthetics. From stunning floral arrangements to thematic decor elements, they work
+                                    closely with
+                                    clients to bring their vision to life.</p>
+                                <a href="#" class="ser-btn">Explore</a>
+
+                            </div>
+                        </div>
+
+
+                        <div class="card mt-5" style="width: 18.5rem;">
+                            <div class="our-services-col2">
+                                <img src="images/ph.jpg">
+                                <h3 class="card-title">Photographers</h3>
+                                <p class="card-text">Our skilled photographers capture the essence of each event through
+                                    their lenses. They have an
+                                    eye
+                                    for detail and an artistic approach, ensuring that every special moment, from candid
+                                    shots to
+                                    key
+                                    highlights, is documented beautifully. Our team of technicians and audiovisual
+                                    specialists
+                                    brings
+                                    technical expertise to the table.</p>
+                                <a href="#" class="ser-btn">Explore</a>
+
+                            </div>
+                        </div>
+
+                        <div class="card mt-5" style="width: 18.5rem;">
+                            <div class="our-services-col2">
+                                <img src="images/cat.jpeg">
+                                <h3 class="card-title">Food Services</h3>
+                                <p class="card-text">Caterers and food service professionals excels in creating culinary
+                                    experiences that delight
+                                    attendees. They carefully consider dietary preferences and
+                                    event
+                                    themes. With a focus on quality ingredients and impeccable presentation, our
+                                    caterers ensure
+                                    that
+                                    guests are treated to an exceptional dining experience.</p>
+                                <a href="#" class="ser-btn">Explore</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </section>
+    </main>
+
+    <!-- footer -->
+    <footer class="navbar-dark nav-bg mt-auto pb-5">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="single-box">
+                        <a href="index.php"><img src="images/logo.png" class="img-fluid img-thumbnail" alt="logo"
+                                style="height: 90px; width: 220px;"></a>
+
+                        <h3 class="pt-4 foot">About Us</h3>
+                        <p class="foot">We are your trusted event planning partner, offering comprehensive services and
+                            tools to streamline your event management process. Let us help you create unforgettable
+                            experiences.</p>
+                        <div class="card-area">
+                            <i class="fa fa-cc-visa foot"></i>
+                            <i class="fa fa-credit-card foot"></i>
+                            <i class="fa fa-cc-mastercard foot"></i>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="single-box">
+                        <h2 class="foot">Quick Links</h2>
+                        <ul>
+                            <li><a href="index.php" class="foot foott-link">Home</a></li>
+                            <li><a href="about_us.php" class="foot foott-link">About Us</a></li>
+                            <!-- <li><a href="#" class="foot foott-link">Categories</a></li> -->
+                            <li><a href="services.php" class="foot foott-link">Services</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="single-box">
+                        <h2 class="foot">Quick Links</h2>
+                        <ul>
+                            <li><a href="portfolio.php" class="foot foott-link">Portfolio</a></li>
+                            <li><a href="contact_us.php" class="foot foott-link">Contact Us</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="single-box">
+                        <h2 class="foot">Contact</h2>
+                        <ul class="widget_contacts_style_1">
+                            <li class="widget_contacts_address text-dark d-flex mt-4">
+                                <div class="icon"><i class='fa-solid fa-location-dot'></i></div>
+                                <div class="text ms-3"><b>Evento (Pvt) Ltd.</b><br><b>Colombo Branch</b><br>No. 47,
+                                    Ananda
+                                    Coomaraswamy Mawatha, Colombo 03, 00300.<br><b>Ja-Ela Branch</b><br>
+                                    No. 78/5(1st Floor),
+                                    Ja-Ela Road Ekala,<br>
+                                    Sri Lanka.
+                                </div>
+                            </li>
+
+                            <li class="widget_contacts_phone text-dark d-flex mt-4">
+                                <div class="icon"><i class='fas fa-phone-alt'></i></div>
+                                <div class="text ms-3"><b>Office phone</b> <br>
+                                    0111234567<br><b>Mobile</b><br>0771234567<br>0701234567<br>0777654321</div>
+                            </li>
+
+                            <li class="widget_contacts_email d-flex align-items-center">
+                                <div class="icon"><i class='fa fa-envelope'></i></div>
+                                <div class="text ms-3">
+                                    <a href="mailto:evento@lanka.lk" class="text-primary">evento@lanka.lk</a>
+                                </div>
+                            </li>
+                        </ul>
+
+                        <!-- <div class="input-group mb-3 mt-3">
+                            <input type="text" class="form-control" placeholder="Recipient's username"
+                                aria-label="Enter your Email ..." aria-describedby="basic-addon2">
+                            <span class="input-group-text" id="basic-addon2">
+                                <i class="fa fa-long-arrow-right"></i>
+                            </span>
+                        </div> -->
+
+                        <div>
+                            <h2 class="text-dark">Follow us on</h2>
+                            <p class="socials text-dark">
+                                <a href="#" class="follow"><i class="fa fa-facebook"></i></a>
+                                <a href="#" class="follow"><i class="fa fa-twitter"></i></a>
+                                <a href="#" class="follow"><i class="fa fa-linkedin"></i></a>
+                                <a href="#" class="follow"><i class="fa fa-instagram"></i></a>
+                                <a href="#" class="follow"><i class="fa fa-youtube"></i></a>
+                                <a href="#" class="follow"><i class="fa fa-pinterest"></i></a>
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="textwidget custom-html-widget">
+            <p style="text-align:center; font-size:15px;">
+                Copyright © 2023 EVENTO (Pvt) Ltd. All rights reserved.
+            </p>
+        </div>
+
+
+
+    </footer>
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossorigin="anonymous"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"
+        integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS"
+        crossorigin="anonymous"></script> -->
+    <script src="/services.js"></script>
+
+
+</body>
+
+
+
+</html>
